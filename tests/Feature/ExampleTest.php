b85f78d506e052522a2d867b211ac63636f90476
Jason McCreary <jason@pureconcepts.net>
2023-02-10 14:30:28 -0500

    Setup MySQL

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,17 +3,26 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+        $user = User::factory()->create();
+
         $response = $this->get('/');
 
         $response->assertStatus(200);
+
+        $this->assertNotNull($user->id);
+        $this->assertSame(1, User::all()->count());
     }
 }
